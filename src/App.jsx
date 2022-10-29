2b02a40431ea017b27a3cf1772aafc93aa3a2aae
jayadhav0710 <110915141+jayadhav0710@users.noreply.github.com>
2022-10-29 15:09:42 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+function App() {
+  return (
+    <div className='App'>
+    <h1>About Us</h1>
+    <p id='firstP'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus, hic?</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni laborum assumenda perspiciatis fugiat saepe rerum minima rem aliquid atque mollitia quidem alias quos deleniti autem harum voluptas ad sint possimus ea, ullam dolores accusantium nesciunt! Commodi, quae sunt temporibus natus amet ducimus numquam iusto facere quod distinctio molestiae obcaecati quis asperiores iste, doloribus suscipit alias aut enim illo. Laudantium nam dolores minus illo ab, inventore facere! Tempore, laborum corrupti asperiores placeat doloremque in illum magni temporibus ratione, nam ut eveniet hic laboriosam iure cumque, praesentium sapiente id quasi numquam deleniti quis? Nostrum in sit eaque quasi dolore natus quas laudantium?</p>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius aliquam amet fugiat, aspernatur velit adipisci cumque vitae, non, eum inventore expedita. Quo, totam eos. Illum maiores corporis eligendi reprehenderit eaque explicabo quas accusantium odit sunt temporibus! Totam quod maiores facere odio unde earum consectetur reprehenderit ullam alias facilis, necessitatibus ipsam tenetur dolores nulla eius ex, eos quia quos, at iste___</p>
+    
+    </div>
+  );
+}
+
+export default App;
